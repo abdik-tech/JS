6ad0c50a236d843c066eff2ecf6527d33313a2c5
abdik-tech <abdihamburge@gmail.com>
2023-11-03 19:31:10 +0300

    souvenirHunt

--- 01-Fundamentals-Part-1/Starter/script.js
+++ 01-Fundamentals-Part-1/Starter/script.js
@@ -17,3 +17,19 @@
 
 // const birthYear = 1990;
 // console.log(birthYear);
+const now = 2023;
+const ageAbdi = now - 1992;
+const ageKhan = now - 1991;
+console.log(ageAbdi, ageKhan);
+console.log(ageKhan * ageAbdi);
+// exponentiation
+console.log(ageKhan ** ageAbdi);
+// concatination
+console.log('ageKhan' + ' ' + 'ageAbdi');
+// assignment Operators
+let x = 10;
+x += 10;
+console.log(x);
+// comparison Operators
+console.log(ageAbdi === ageKhan);
+
---
+++ algorithm/apartmentHunt.js
@@ -0,0 +1,25 @@
+//rough sketch of code
+
+// function souvenirDirection is in relation to the direction of EPS head Quarter which is around the Ambassador area
+function souvenirDirection(EPS, reqs) {
+
+    if (req === 'top') {
+        console.log('Shop Located North of EPS Head Quarter');
+    } else if (req === 'bottom') {
+        console.log('Shop Located South of EPS Head Quarter');
+    } else if (req === 'left') {
+        console.log('Shop Located West of EPS Head Quarter');
+    } else if (req === 'right') {
+        console.log('Shop Located East of EPS Head Quarter');
+    } else {
+        console.log('Invalid Direction');
+    }
+
+}
+
+
+
+function souvenirHunting(direction) {
+    //code to be executed
+
+}
