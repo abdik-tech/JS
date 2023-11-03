fcd888375cd81cd2a4fd36e1d5bddc8eb6be675d
abdik-tech <abdihamburge@gmail.com>
2023-11-03 18:01:46 +0300

    toOPerators

--- 01-Fundamentals-Part-1/Starter/script.js
+++ 01-Fundamentals-Part-1/Starter/script.js
@@ -1,3 +1,19 @@
-let firstName = "Abdi";
-let lastName = "Kitesa";
-console.log(firstName, lastName);
+// let firstName = "Abdi";
+// let lastName = "Kitesa";
+// console.log(firstName, lastName);
+
+// let JavaScriptIsFun = true;
+// console.log(typeof JavaScriptIsFun);
+
+// let firstName = "Abdi";
+// console.log(typeof firstName);
+
+// let year;
+
+// console.log(typeof year);
+// year = 2020;
+
+// console.log(typeof year);
+
+// const birthYear = 1990;
+// console.log(birthYear);
