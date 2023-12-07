74b0ed59fe3ac06b81be9a3f9b0125e98178f060
abdik-tech <abdihamburge@gmail.com>
2023-12-07 17:35:58 +0300

    lectures

---
+++ 07-PIG GAME/starter/script.js
@@ -0,0 +1,6 @@
+'use strict';
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+// const score0 = document.getElementById('score--0');
+const score1El = document.querySelector('#score--1');
+// const score1 = document.getELementById('score--1');
